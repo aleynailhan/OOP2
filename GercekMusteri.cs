5bf0abda9262f0dccf795f898c6377c1b0d07892
aleynailhannn@gmail.com <aleynailhannn@gmail.com>
2021-02-20 15:29:57 +0300

    Proje dosyası ekle.

---
+++ GercekMusteri.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class GercekMusteri:Musteri //bu olaya inheritance denir
+    {
+        
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public string TcNo { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "aleyna";
+            musteri1.Soyadi = "ilhan";
+            musteri1.TcNo = "2222222222";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54565";
+            musteri2.SirketAdi = "kodlama.io";
+            musteri2.VergiNo = "123354590";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri(); //musteri classı hem gercek musterinin hem de tuzel musterinin referansını tutabiliyor.
+            CustomerManager customer = new CustomerManager();
+            customer.Add(musteri1);
+            customer.Add(musteri2);
+        }
+    }
+}
